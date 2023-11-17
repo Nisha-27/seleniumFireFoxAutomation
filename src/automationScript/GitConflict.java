086367f97a7a9ae696eeddc59922896bf12fd918
Nisha-27 <nisharavi2712@gmail.com>
2023-11-17 14:05:35 +0530

    resolved conflict

--- src/automationScript/GitConflict.java
+++ src/automationScript/GitConflict.java
@@ -4,7 +4,11 @@ public class GitConflict {
 	
 	public static void main(String[] args) {
 		
+
 		System.out.println("in remote - master branch");
+
+		System.out.println("in local - master branch");
+
 	}
 
 }
